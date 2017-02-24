b098d62ff279c4554f77c3677622073033d067d5
DebshreeC <debshreechakraborty@yahoo.co.in>
2017-02-24 19:56:41 +0530

    Add files via upload

---
+++ ImportModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBIplus.BAL.ViewModels
+{
+    public class ImportModel
+    {
+        
+        public List<string> GridHeaders { get; set; }
+        public List<string> UniqueBatchList { get; set; }
+       
+        public List<string> Headers { get; set; }
+       
+        public DateTime? DOS { get; set; }
+
+        //High Mark Common Fileds
+
+
+
+//        public	BATCH_ID	Int
+//public	ACCOUNT_NO	Varchar(500)
+//public	DOS	DateTime
+//public	CLIENT_ID	Int
+//public	DENIAL_WORKED_DT	DateTime
+//public	ERROR_CATEGORY	Varchar(50)
+//public	SUB_CATEGORY	Varchar(50)
+//public	CODER_LOGIN_ID	Varchar(50)
+//public	QC_LOGIN_ID	Varchar(50)
+//public DENIAL_TYPE	Varchar(50)
+//public	COMMENTS	Varchar(100)
+//public	AUDITOR_NAME	Varchar(50)
+//public	EMP_ID	Int
+//public	EMP_NAME	Varchar(50)
+//public	LOCATION_ID	Int
+//public	PROJECT_ID	Int
+//public	PRACTICE_ID	Int
+
+       
+    }
+}
---
+++ UserMenuModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace McKesson_Denial_Coding.BAL.ViewModels
+{
+    public class UserMenuModel
+    {
+        public string UserName { get; set; }
+        ////public List<string> ListOfMainMenu { get; set; }
+        ////public List<string> ListOfSubMenu { get; set; }
+        //public string MainMenu { get; set; }
+        //public string SubMenu { get; set; }
+        public List<SelectListItem> ProjectList { get; set; }
+        public string SelectedProject { get; set; }
+        public List<SelectListItem> LocationList { get; set; }
+        public string SelectedLocation { get; set; }
+    }
+}
